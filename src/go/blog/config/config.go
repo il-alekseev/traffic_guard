cb2b776e97b51d2c95d548e91b61c64627c0d021
voronhub <vovaoryole@gmail.com>
2025-11-16 22:34:29 +0300

    Обновлен Dockerfile сервиса blog

--- src/go/blog/config/config.go
+++ src/go/blog/config/config.go
@@ -2,7 +2,9 @@ package config
 
 import (
 	"errors"
+	"flag"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/ilyakaznacheev/cleanenv"
@@ -61,14 +63,49 @@ type (
 func NewConfig() (*Config, error) {
 	cfg := &Config{}
 
-	// Сначала читает параметры из конфига - удобно при разработке и локальном запуске.
-	// Затем смотрит переменные окружения и перезаписывает параметры из конфига.
-	// Таким образом параметры ENV имеют приоритет над конфигом - удобно при развертывании
-	// в docker
-	err := cleanenv.ReadConfig("./config/config.yml", cfg) //./config/config.yml   fiermon/fiermon-api-gw/config/config.yml
+	configPath, err := fetchConfigPath()
 	if err != nil {
-		return nil, fmt.Errorf("config error: %w", err)
+		fmt.Println("config file not found, using environment variables")
+
+		err = cleanenv.ReadEnv(cfg)
+		if err != nil {
+			return nil, fmt.Errorf("error reading environment variables: %w", err)
+		}
+
+		return cfg, nil
+	}
+
+	err = cleanenv.ReadConfig(configPath, cfg)
+	if err != nil {
+		return nil, fmt.Errorf("error reading config file: %w", err)
+	}
+
+	// Чтение переменных которые не указаны в конфигурационном файле или перезапись переменных
+	// Необходимо для передачи парольной информации, когда основная информация в конфигурационном файле,
+	// а парольная информация передается через переменное окружение
+	err = cleanenv.ReadEnv(cfg)
+	if err != nil {
+		return nil, err
 	}
 
 	return cfg, nil
+}
+
+// fetchConfigPath - получение пути конфигурационного файла
+func fetchConfigPath() (string, error) {
+	var path string
+
+	flag.StringVar(&path, "config", "config/configs.yml", "config file path")
+	flag.Parse()
+
+	if _, err := os.Stat(path); err != nil {
+		path = os.Getenv(configPathEnvName)
+	}
+
+	_, err := os.Stat(path)
+	if err != nil {
+		return "", err
+	}
+
+	return path, nil
 }
