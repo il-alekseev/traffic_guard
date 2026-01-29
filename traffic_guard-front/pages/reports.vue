20e7ec9cd0bbc61622dab65e0b7377f309ce6c22
ForeverScrag24 <nepar2000@bk.ru>
2026-01-29 16:34:39 +0300

    UPD: Frontend BUGS

--- traffic_guard-front/pages/reports.vue
+++ traffic_guard-front/pages/reports.vue
@@ -128,6 +128,7 @@ import AnalyticsByDevice from '~/components/report/AnalyticsByDevice.vue'
 import AnomaliesTableByDevice from '~/components/report/AnomaliesTableByDevice.vue';
 import AnomaliesRating from '~/components/report/AnomaliesRating.vue';
 import CategoriesRating from '~/components/report/CategoriesRating.vue';
+import { normalizeEndDate, normalizeStartDate } from '~/helpers';
 
 
 definePageMeta({
@@ -197,7 +198,7 @@ const getReport = async (formData: ReportFormData) => {
 
   if (formData.deviceSelection === 'specific' && formData.selectedDevice) {
     try {
-      const reportDataResponse = await reportStore.fetchReportByDevice(new Date(formData.dateFrom)?.toISOString(), new Date(formData.dateTo)?.toISOString(), formData.selectedDevice);
+      const reportDataResponse = await reportStore.fetchReportByDevice(normalizeStartDate(new Date(formData.dateFrom)).toISOString(), normalizeEndDate(new Date(formData.dateTo)).toISOString(), formData.selectedDevice);
       reportDataByDevice.value = reportDataResponse;
       reportDataDeviceName.value = formData.selectedDevice;
       setReportConfig(reportDataByDevice.value.from, reportDataByDevice.value.to, reportDataByDevice.value.hostname);
@@ -212,7 +213,7 @@ const getReport = async (formData: ReportFormData) => {
     }
   } else {
     try {
-      const reportDataResponse = await reportStore.fetchReportAllDevices(new Date(formData.dateFrom)?.toISOString(), new Date(formData.dateTo)?.toISOString());
+      const reportDataResponse = await reportStore.fetchReportAllDevices(normalizeStartDate(new Date(formData.dateFrom)).toISOString(), normalizeEndDate(new Date(formData.dateTo)).toISOString());
       reportData.value = reportDataResponse;
       setReportConfig(reportData.value.from, reportData.value.to);
       formSucces.value = true;
@@ -296,7 +297,11 @@ const downloadReport = async () => {
       
     }
 
-    const fileName = `report_${new Date().toISOString().split('T')[0]}.pdf`
+
+    let fileName = `report_${new Date().toISOString().split('T')[0]}.pdf`
+    if (reportDataDeviceName.value) {
+      fileName = `report_${reportDataDeviceName.value.toLowerCase()}_${new Date().toISOString().split('T')[0]}.pdf`
+    }
     pdf.save(fileName)
     
   } catch (error) {
